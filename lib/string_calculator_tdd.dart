1280a07c8baf156e1d912436ca95c38e7ff30ca6
Manoharbablu <manohar.professional.flutter@com>
2025-07-23 17:27:08 +0530

    feat: handle single number input

--- lib/string_calculator_tdd.dart
+++ lib/string_calculator_tdd.dart
@@ -1,4 +1,4 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
-  return 0; // Step 1: handle empty string
+  return int.parse(numbers);
 }
--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -1,8 +1,12 @@
-import 'package:string_calculator_tdd/string_calculator_tdd.dart';
 import 'package:test/test.dart';
+import 'package:string_calculator_tdd/string_calculator_tdd.dart';
 
 void main() {
   test('Empty string returns 0', () {
     expect(add(""), equals(0));
+  });
+
+  test('Single number returns the number itself', () {
+    expect(add("5"), equals(5));
   });
 }
